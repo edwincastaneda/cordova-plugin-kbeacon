6dc172373593c69791902bf01ab7984ae1444919
Edwin Castañeda <edwincastaneda@users.noreply.github.com>
2023-06-12 00:29:38 -0600

    Update cordovaPluginKBeacon.swift

--- src/ios/cordovaPluginKBeacon.swift
+++ src/ios/cordovaPluginKBeacon.swift
@@ -60,7 +60,7 @@ public class cordovaPluginKBeacon : CDVPlugin, KBeaconMgrDelegate{
                     KBArray.append(String(iBeaconAdv.minorID))
                     KBArray.append(String(iBeaconAdv.majorID))
 
-                    cordovaPluginKBeacon.mBeaconsDictory[String(advBeacon.minorID)!] = KBArray
+                    cordovaPluginKBeacon.mBeaconsDictory[advBeacon.minorID!] = KBArray
                 }
 
             default:
